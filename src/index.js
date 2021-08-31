e4e4434429d99f44904402603ff4c7073756da10
Ángel Díaz <diazangel9311@gmail.com>
2021-08-31 17:08:59 -0500

    Add basic layout

--- src/index.js
+++ src/index.js
@@ -1,14 +1,2 @@
-import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('h1');
-
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  return element;
-}
-
-document.body.appendChild(component());
