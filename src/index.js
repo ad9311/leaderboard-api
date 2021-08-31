8dc39e0e0d69f61f5030b258d5c6766a6922247d
Ángel Díaz <diazangel9311@gmail.com>
2021-08-31 18:14:27 -0500

    Add events for adding and redering localstorage

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 import './style.css';
+import lb from './leaderboard.js';
+import dom from './dom.js';
 
+const renderOnLoad = () => {
+  dom.renderStorage(lb);
+}
+
+const addNewScore = () => {
+  lb.addNewScore(dom.userData());
+  dom.updateLocalStorage(lb);
+  dom.renderScore();
+  dom.cleanFields();
+}
+
+window.addEventListener('load', renderOnLoad());
+document.getElementById('add-score').addEventListener('click', () => addNewScore());
